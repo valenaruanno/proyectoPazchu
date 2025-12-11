b4837f36f35dd1e66271328e4ff8112a35fbe5ae
valenaruanno <valentinaruanno2004@gmail.com>
2025-12-11 16:44:35 -0300

    arrego el archivo nixpacks.toml

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x mvnw
-./mvnw clean package -DskipTests -Dmaven.compiler.release=21 -f englishteacher-api/pom.xml
-java -Dserver.port=${PORT:-8080} -jar englishteacher-api/target/englishteacher-api-0.0.1-SNAPSHOT.jar
